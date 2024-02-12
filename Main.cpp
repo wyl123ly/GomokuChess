890ad0d60de5082a3837fc1a7e4f002494b23cdd
wyl123ly <141922286+wyl123ly@users.noreply.github.com>
2024-02-12 11:47:43 +0800

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -3,6 +3,7 @@
 #include<algorithm>
 #include<windows.h>
 #include<conio.h>
+#include<vector>
 using namespace std;
 class Chess{
 public:
@@ -33,13 +34,17 @@ public:
 	1 Black
 	-1 White
 	*/
+	struct ChessToVec{
+		int type,x,y;
+	};
+	vector<ChessToVec> AllChess;
 	bool ChooseVision = true;
 	int Cv = 1;
 	void _GetKey(bool &Keyflag/*To judge if 'c' is pushed*/,bool &IfKey/*To judge if _kbhit()*/){
 		if(_kbhit()){
 			IfKey = true;
 			char c = _getch();
-			if(c == 'c'){
+			if(c == 'c' || c == ' '){
 				Keyflag = true;
 			}
 			if(c == 'v'){//Control the choose vision
@@ -94,8 +99,24 @@ public:
 				}
 				
 				if(!ChooseVision){
+					if(i == SizeOf + 1 && j == SizeOf + 1){
+						printf("  "); break;
+					}else if(i == SizeOf + 1 && j == PlayerChooseX){
+						printf("* "); break;
+					}else if(i == PlayerChooseY && j == SizeOf + 1){
+						printf("* "); break;
+					}else if(i == SizeOf + 1 || j == SizeOf + 1){
+						printf("  ");
+					}
+
 					if(i == PlayerChooseY && j == PlayerChooseX){
-						printf("+ ");
+						if(ChB[i][j] == 1){
+							printf("& ");
+						}else if(ChB[i][j] == -1){
+							printf("# ");
+						}else{
+							printf("+ ");
+						}
 						continue;
 					}
 				}
@@ -132,14 +153,59 @@ public:
 private:
 	int PlayerChooseX = 1,PlayerChooseY = 1;
 };
-class Judge{
-public:
-	bool flag = true;
-private:
-};
 ChessBoard cb;
 Chess chess[10005];
+class Judge{
+public:
+	int IfWinner;
+	struct DIRECTION{
+		int Xmove,Ymove;
+	};
+	DIRECTION Move[10] = {{0,0},{1,1},{0,1},{-1,1},{-1,0},{-1,-1},{0,-1},{1,-1},{1,0}};
+	bool flag = true;
+	int Winner;/*0 --> Draw 1--> Black -1 --> White*/
+	bool IfWin(int xpos,int ypos){
+		int TypeOf = cb.ChB[xpos][ypos];
+		for(int dire = 1;dire <= 8;dire++){
+			bool IfWin = true;
+			int x = xpos,y = ypos;
+			for(int l = 1;l <= 4;l++){
+				x += Move[dire].Xmove;
+				y += Move[dire].Ymove;
+				if(x < 1 || x > cb.SizeOf || y < 1 || y > cb.SizeOf){
+					IfWin = false;
+					break;
+				}
+				if(cb.ChB[x][y] != TypeOf){
+					IfWin = false;
+					break;
+				}
+			}
+			if(IfWin){
+				return true;
+			}
+		}
+		return false;
+	}
+private:
+};
 Judge Judgement;
+class ComputerPlayer{
+public:
+private:
+};
+ComputerPlayer ComP;
+void EndOfGame(){
+	if(Judgement.IfWinner == 1){
+		system("cls");
+		cb.PrintChessBoard();
+		printf("Black Wins!");
+	}else{
+		system("cls");
+		cb.PrintChessBoard();
+		printf("White Wins!");
+	}
+}
 int main(){
 	scanf("%d", &cb.SizeOf);
 	while(Judgement.flag){
@@ -160,15 +226,26 @@ int main(){
 				}else{
 					if(StepOf % 2 == 0){
 						chess[StepOf].CreateChessB(cb.PlcY(),cb.PlcX());
+						cb.AllChess.push_back({type : 1,x : cb.PlcY(),y : cb.PlcX()});
 					}else{
 						chess[StepOf].CreateChessW(cb.PlcY(),cb.PlcX());
+						cb.AllChess.push_back({type : -1,x : cb.PlcY(),y : cb.PlcX()});
 					}
 					cb.PutChess(chess[StepOf].TypeOf(),cb.PlcY(),cb.PlcX());
+					int cnt = 0;
+					while(cnt * 1ull < cb.AllChess.size()){
+						Judgement.IfWinner = cb.AllChess[cnt].type;
+						if(Judgement.IfWin(cb.AllChess[cnt].x,cb.AllChess[cnt].y)){
+							Judgement.flag = false;
+						}
+						cnt++;
+					}
 				}
 				
 			}
 		}
 		cb.Step++;     
 	}
+	EndOfGame();
 	return 0;
 }
